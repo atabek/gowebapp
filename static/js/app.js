9a92ec98a8236b31b2d3f31b42e9753b8c8aabc8
Shahmah <3x_ata@rambler.ru>
2016-04-10 22:19:16 -0500

    Added listing students on /list

--- static/js/app.js
+++ static/js/app.js
@@ -6,9 +6,38 @@ document.getElementById("demo").innerHTML = "My First JavaScript";
 //         $interpolateProvider.endSymbol(']]');
 //     });
 
-var app = angular.module('myApp', []);
+var app = angular.module('aftercareApp', []);
 
-app.controller('myCtrl', function($scope) {
-    $scope.firstName= "John";
-    $scope.lastName= "Doe";
+app.controller('StudentCtrl', function($scope, $http, $filter) {
+    'use strict';
+
+    $scope.firstName = "Atabek";
+    $scope.lastName  = "Akbalaev";
+
+    $scope.students = [];
+    $scope.errors = [];
+    $scope.search = {};
+    $scope.filteredFields = ['Student_id', 'Last_name', 'First_name', 'Grade'];
+    $scope.filteredStudents = [];
+
+    $scope.refilter = function() {
+        for (var f in $scope.filteredFields) {
+            var field = $scope.filteredFields[f];
+            if ($scope.search[field] === '') delete $scope.search[field];
+        }
+        $scope.filteredStudents = $filter('filter')($scope.students, $scope.search);
+    };
+
+
+    $http.get('/students').then(function(res) {
+        $scope.students = res.data;
+        console.log($scope.students);
+        $scope.refilter();
+    }, function(msg) {
+        $scope.log(msg.data);
+    });
+
+    $scope.log = function(msg) {
+        $scope.errors.push(msg);
+    };
 });
