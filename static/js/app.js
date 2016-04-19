d476eddbf590221c36356fd5d232590f7fca8ac7
Shahmah <3x_ata@rambler.ru>
2016-04-18 19:46:16 -0500

    Added single filter field

--- static/js/app.js
+++ static/js/app.js
@@ -1,33 +1,22 @@
-document.getElementById("demo").innerHTML = "My First JavaScript";
-
 // var app = angular.module('myApp', [],
 //     function($interpolateProvider){
 //         $interpolateProvider.startSymbol('[[');
 //         $interpolateProvider.endSymbol(']]');
 //     });
-
 var app = angular.module('aftercareApp', []);
 
-app.controller('StudentCtrl', function($scope, $http, $filter) {
+app.controller('StudentCtrl', function($scope, $http) {
     'use strict';
-
-    $scope.firstName = "Atabek";
-    $scope.lastName  = "Akbalaev";
 
     $scope.students = [];
     $scope.errors = [];
-    $scope.search = {};
-    $scope.filteredFields = ['Student_id', 'Last_name', 'First_name', 'Grade'];
-    $scope.filteredStudents = [];
 
-    $scope.refilter = function() {
-        for (var f in $scope.filteredFields) {
-            var field = $scope.filteredFields[f];
-            if ($scope.search[field] === '') delete $scope.search[field];
-        }
-        $scope.filteredStudents = $filter('filter')($scope.students, $scope.search);
+    $scope.search = function (row) {
+        return (angular.lowercase(row.Student_id).indexOf(angular.lowercase($scope.query) || '') !== -1 ||
+                angular.lowercase(row.Last_name).indexOf(angular.lowercase($scope.query)  || '') !== -1 ||
+                angular.lowercase(row.First_name).indexOf(angular.lowercase($scope.query) || '') !== -1 ||
+                angular.lowercase(row.Grade).indexOf(angular.lowercase($scope.query)      || '') !== -1);
     };
-
 
     $http.get('/students').then(function(res) {
         $scope.students = res.data;
