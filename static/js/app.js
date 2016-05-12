a7076b25a09194077fedb98694b0dd95e59e44aa
Shahmah <3x_ata@rambler.ru>
2016-05-12 02:16:08 -0500

    added date filtering

--- static/js/app.js
+++ static/js/app.js
@@ -11,16 +11,12 @@ angular.module('aftercareApp').factory('ClockinService', function($resource){
         {query: {
             isArray: true,
             method: 'GET',
-            headers: {'X-Api-Secret': 'xxx', 'Authorization': 'xxx', 'Content-Type': 'application/x-www-form-urlencoded'}
+            headers: {'Content-Type': 'application/json'}
         }
     });
 })
 
-.filter("dateRangeFilter", function(){
-
-})
-
-.controller('StudentCtrl', function($scope, $http) {
+.controller('StudentCtrl', ['$scope', '$http', function($scope, $http) {
     'use strict';
 
     $scope.students = [];
@@ -34,26 +30,60 @@ angular.module('aftercareApp').factory('ClockinService', function($resource){
 
     $http.get('/students').then(function(res) {
         $scope.students = res.data;
+        console.log($scope.students);
     }, function(msg) {
         $scope.log(msg.data);
     });
-})
+}])
 
-.controller('ClockinCtrl', function($scope, $http , ClockinService, $location){
+.controller('ClockinCtrl', ['$scope', '$http', 'ClockinService', '$location',
+            function($scope, $http , ClockinService, $location){
     var url = $location.absUrl().split('/');
     var studentID = url[url.length - 1]
     var Clockins = ClockinService.query({id: studentID});
     Clockins.$promise.then(function(data){
-        //$scope.Clockins = angular.toJson(data);
+        console.log(typeof data);
         data = data.splice(-2, 2);
         $scope.Clockins = data;
+    }, function(error){
+        console.log('oopps ' + error);
     });
 
     var tf = new Date();
 
     $scope.dateRange = {
-        from: new Date(tf.setDate(tf.getDate() - tf.getDate() + 1)),
+        from: new Date(tf.setDate(1)),
         to:   new Date()
     };
-    console.log(typeof $scope.dateRange.from);
+}])
+
+.filter("dateRangeFilter", function(){
+    return function(items, from, to){
+        var result = [];
+        if(typeof items == "undefined"){
+            return result;
+        }
+        var df = parseDate(from);
+        var dt = parseDate(to);
+        console.log(df);
+        // console.log(Object.keys(items).length);
+        // console.log("is it an array: " + items[0] instanceof Array);
+        for (var i = 0; i < items.length; i++){
+            var tf = new Date(items[i].InAt * 1000),
+                tt = new Date(items[i].OutAt * 1000);
+            if (tf > df && tt < dt)  {
+                result.push(items[i]);
+            }
+        }
+        return result;
+    };
 });
+
+function parseDate(input) {
+    var day = input.getDate();
+    var month = input.getMonth();
+    var year = input.getFullYear();
+    d = new Date(year, month, day);
+
+    return d.getTime();
+}
